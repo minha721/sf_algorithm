9f2bb446106b52117aa5532597cdeaeb8a521a95
minha721 <2minha721@gmail.com>
2022-09-08 11:07:15 +0900

    [ADD] Solve 1242

--- swea/1242_암호코드스캔.py
+++ swea/1242_암호코드스캔.py
@@ -11,48 +11,61 @@ ratio = {
     (1, 1, 2): 9,
 }
 
+binary = {
+    '0':'0000', '1':'0001', '2':'0010', '3':'0011',
+    '4':'0100', '5':'0101', '6':'0110', '7':'0111',
+    '8':'1000', '9':'1001', 'A':'1010', 'B':'1011',
+    'C':'1100', 'D':'1101', 'E':'1110', 'F':'1111'
+}
+
 def check_pass(pwd):
     pwd = list(map(int, pwd))
     num = 0
     for i in range(8):
-        if i % 2 == 0:
+        if i % 2 == 1:
             num += (3 * pwd[i])
         else:
             num += pwd[i]
 
-    return num, sum(pwd)
+    if num % 10 == 0:
+        return True
+    else:
+        return False
 
 T = int(input())
 for tc in range(T):
     n, m = map(int, input().split())
-    arr = sorted(list(set([input().rstrip() for _ in range(n)])))[1:]
+    arr = sorted(list(set([input().strip() for _ in range(n)])))[1:]
 
-    pwd = []
-    for n in arr:
-        binary = format(int(n, 16), 'b').strip('0')
-        n1 = n2 = n3 = 0
+    visited = []
+    answer = 0
+
+    for a in arr:
+        b = []
+        for i in range(len(a)):
+            b.append(binary[a[i]])
+
+        b = ''.join(b).rstrip('0')
+        n1, n2, n3, n4 = 0, 0, 0, 0
         password = []
-        for i in range(len(binary)):
-            if binary[i] == '1' and n2 == 0:
-                n1 += 1
-            elif binary[i] == '0' and n1 != 0 and n3 == 0:
-                n2 += 1
-            elif binary[i] == '1' and n2 != 0:
-                n3 += 1
-            elif binary[i] == '0' and n3 != 0:
-                r = min(n1, n2, n3)
-                pw = ratio[(n1//r, n2//r, n3//r)]
-                password.append(pw)
-                n1 = n2 = n3 = 0
 
-            if len(password) == 8:
-                pwd.append(password)
-    print(pwd)
-    # pwd = list(set(pwd))
-    # answer = 0
-    # for p in pwd:
-    #     chk, s = check_pass(p)
-    #     if chk % 10 == 0:
-    #         answer += s
-    #
-    # print('#{} {}'.format(tc + 1, answer))
+        for i in range(len(b) - 1, -1, -1):
+            if b[i] == '1' and n2 == 0:
+                n1 += 1
+            elif b[i] == '0' and n1 != 0 and n3 == 0:
+                n2 += 1
+            elif b[i] == '1' and n2 != 0 and n4 == 0:
+                n3 += 1
+            elif b[i] == '0' and n3 != 0:
+                if b[i - 1] == '1':
+                    r = min(n1, n2, n3)
+                    password.append((ratio[n3 // r, n2 // r, n1 // r]))
+                    n1, n2, n3, n4 = 0, 0, 0, 0
+                    if len(password) == 8:
+                        if check_pass(password):
+                            if password not in visited:
+                                answer += sum(password)
+                                visited.append(password)
+                        password = []
+
+    print('#{} {}'.format(tc + 1, answer))
