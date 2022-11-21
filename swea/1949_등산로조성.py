bfebba8e294c02c6dde41193bc8e8f9c2bd04c9d
minha721 <2minha721@gmail.com>
2022-11-22 01:14:36 +0900

    등산로 조성 수정

--- swea/1949_등산로조성.py
+++ swea/1949_등산로조성.py
@@ -1,26 +1,26 @@
-def dfs(x, y, cut):
+def dfs(y, x, cut):
     global res, visited
-    res = max(res, visited[x][y])
+    res = max(res, visited[y][x])
 
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
+    dx = [0, 0, -1, 1]
 
     for i in range(4):
-        nx = x + dx[i]
         ny = y + dy[i]
+        nx = x + dx[i]
 
-        if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
-            if mountain[x][y] > mountain[nx][ny] :
-                visited[nx][ny] = visited[x][y] + 1
-                dfs(nx, ny, cut)
-                visited[nx][ny] = 0
-            elif cut and mountain[x][y] > mountain[nx][ny] - K:
-                t = mountain[nx][ny]
-                mountain[nx][ny] = mountain[x][y] - 1
-                visited[nx][ny] = visited[x][y] + 1
-                dfs(nx, ny, cut - 1)
-                visited[nx][ny] = 0
-                mountain[nx][ny] = t
+        if 0 <= ny < N and 0 <= nx < N and not visited[ny][nx]:
+            if mountain[y][x] > mountain[ny][nx]:
+                visited[ny][nx] = visited[y][x] + 1
+                dfs(ny, nx, cut)
+                visited[ny][nx] = 0
+            elif cut and mountain[y][x] > mountain[ny][nx] - K:
+                t = mountain[ny][nx]
+                mountain[ny][nx] = mountain[y][x] - 1
+                visited[ny][nx] = visited[y][x] + 1
+                dfs(ny, nx, cut - 1)
+                visited[ny][nx] = 0
+                mountain[ny][nx] = t
 
 T = int(input())
 
@@ -34,9 +34,9 @@ for tc in range(1, T + 1):
 
     peak = [(i, j) for i in range(N) for j in range(N) if mountain[i][j] == highest]
 
-    for x, y in peak:
-        visited[x][y] = 1
-        dfs(x, y, 1)
-        visited[x][y] = 0
+    for y, x in peak:
+        visited[y][x] = 1
+        dfs(y, x, 1)
+        visited[y][x] = 0
 
     print("#{} {}".format(tc, res))
